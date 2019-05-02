16770ac3e96bb0c1b39311e246441884a758b853
hassanio <20100056@lums.edu.pk>
2019-05-02 22:41:59 +0500

    ll

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import keys from './config/keys'
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
