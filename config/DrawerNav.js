a8b9d9bcc33043a5fb3bc59823b1d8187270442f
Mustafa <mustafa_abbasi4@hotmail.com>
2019-05-02 18:11:53 +0500

    Added approved donations in drawer

--- config/DrawerNav.js
+++ config/DrawerNav.js
@@ -7,11 +7,13 @@ import D_Profile from '../screens/D_Profile'
 import Help from '../screens/Help'
 import D_Form from '../screens/D_Form'
 import CustomDrawer from '../screens/CustomDrawer.js'
+import ApprovedDonations from '../screens/ApprovedDonations.js';
 
 
 const DrawerStack = createDrawerNavigator({
   'My Donations': { screen: FeedStack },
   'My Profile': { screen: D_Profile },
+  'Approved Donations': { screen: ApprovedDonations },
   'Help and Hints': { screen: Help },
   d_form: { screen: D_Form },
 }, {
