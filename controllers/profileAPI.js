c29002bc15e8adf801f28f8cf4b290493c4a8258
Mustafa <mustafa_abbasi4@hotmail.com>
2019-05-07 01:47:40 +0500

    Update Profile now checking if image present or not

--- controllers/profileAPI.js
+++ controllers/profileAPI.js
@@ -35,30 +35,34 @@ exports.fetchImage = async (req, res) => {
 exports.updateProfile = async (req, res) => {
     const isDonor = req.authInfo == roles.Donor
     const User = isDonor ? Donor : NGO
-
-    //Base-64 encode the image
-    const dUri = new DataUri()
-    dUri.format(path.extname(req.file.originalname), req.file.buffer)
+    
+    const isImage = req.file && req.file.buffer
 
     try {
         const user = await User.findById(req.user.id)
         const public_img_id = user.image_id
 
-        //V.IMP: ONLY set folder if no public_id already, otherwise, it will create nested folders
-        let upload_options = {}
-        if (public_img_id) {
-            upload_options.public_id = public_img_id
-        } else {
-            upload_options.folder = isDonor ? 'images/donors' : 'images/ngos'
-        }
-        const result = await cloudinary.uploader.upload(dUri.content, upload_options)
-
         //Finally, update donor/ngo
         const updateValues = req.body
-        updateValues.image_id = result.public_id
-        updateValues.image = result.url 
-        await User.findOneAndUpdate({ _id: req.user.id} , updateValues)
 
+        if(isImage) {
+        	const dUri = new DataUri()
+        	//Base-64 encode the image
+        	dUri.format(path.extname(req.file.originalname), req.file.buffer)
+	        //V.IMP: ONLY set folder if no public_id already, otherwise, it will create nested folders
+	        let upload_options = {}
+	        if (public_img_id) {
+	            upload_options.public_id = public_img_id
+	        } else {
+	            upload_options.folder = isDonor ? 'images/donors' : 'images/ngos'
+	        }
+	        const result = await cloudinary.uploader.upload(dUri.content, upload_options)
+
+	        updateValues.image_id = result.public_id
+	        updateValues.image = result.url 
+        }
+
+        await User.findOneAndUpdate({ _id: req.user.id} , updateValues)
         res.send({ success: true })
     }
     catch(err) {
